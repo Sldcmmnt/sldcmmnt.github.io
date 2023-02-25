ccdefe350125261f27268fcd4e4fab8c9f0b0609
GijsvandenBeucken <55687057+GijsvandenBeucken@users.noreply.github.com>
2023-02-25 16:42:54 +0100

    update

---
+++ js/sidebar-maybelater.js
@@ -0,0 +1,29 @@
+
+//select all the elements with .product_section
+//var products = document.querySelectorAll('section');
+//loop through them
+//products.forEach(function(el){
+  //create ul element
+  //var ul = document.createElement('li');
+  //find all h2, h3 from the currrent element
+  //var c = el.querySelectorAll('h1, h3');
+  //loop through them
+  //c.forEach(function(header){
+    //create li element
+
+    //create anchor element
+    //var a = document.createElement('a');  
+    //set the href attribute and set the link text
+   // a.href = '#' + header.textContent.toLowerCase().replace(' ', '-');
+    //a.textContent = header.textContent;
+   
+    //append the li to ul element
+    //ul.appendChild(a);
+ // });
+  //append the ul to div element
+ // document.querySelector('.navigation').appendChild(ul);
+  //remove the current div element
+ 
+//});
+
+
